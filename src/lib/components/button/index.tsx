5b8bcd72d3f9680b6fb86f099bb5c0ffa7b1566d
git-marcopitra <marcomorete73@hotmail.com>
2023-04-07 00:33:09 +0100

    :bug: fix: displayName

--- src/lib/components/button/index.tsx
+++ src/lib/components/button/index.tsx
@@ -1,31 +1,24 @@
 import stylin, { variant } from '@stylin.js/react';
-import React, {
-  FC,
-  forwardRef,
-  PropsWithChildren,
-  PropsWithRef,
-  RefAttributes,
-} from 'react';
+import React, { FC, PropsWithChildren, RefAttributes } from 'react';
 
 import { ButtonElementProps, ButtonProps } from './button.types';
 
-export const Button: FC<PropsWithRef<PropsWithChildren<ButtonProps>>> =
-  forwardRef((props, ref) => {
-    const ButtonElement = stylin<ButtonElementProps & RefAttributes<unknown>>(
-      'button'
-    )(
-      variant({
-        scale: 'buttonVariants',
-        property: 'variant',
-      }),
-      variant({
-        scale: 'buttonSizes',
-        property: 'size',
-      })
-    );
+export const Button: FC<PropsWithChildren<ButtonProps>> = (props) => {
+  const ButtonElement = stylin<ButtonElementProps & RefAttributes<unknown>>(
+    'button'
+  )(
+    variant({
+      scale: 'buttonVariants',
+      property: 'variant',
+    }),
+    variant({
+      scale: 'buttonSizes',
+      property: 'size',
+    })
+  );
 
-    return <ButtonElement {...props} ref={ref} />;
-  });
+  return <ButtonElement {...props} />;
+};
 
 // Button.displayName = 'Button';
 export * from './button.types';
