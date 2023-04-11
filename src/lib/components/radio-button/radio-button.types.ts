89ada20f8b5d91cf137fd4da4db7c692fdd54275
KipandaJr <antoniokipandacardoso@gmail.com>
2023-04-11 02:42:31 +0100

    :fire: feat: remove unused types

--- src/lib/components/radio-button/radio-button.types.ts
+++ src/lib/components/radio-button/radio-button.types.ts
@@ -1,9 +1,4 @@
-import {
-  HTMLAttributes,
-  InputHTMLAttributes,
-  LabelHTMLAttributes,
-  RefAttributes,
-} from 'react';
+import { InputHTMLAttributes, LabelHTMLAttributes } from 'react';
 
 export type RadioButtonElementProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
@@ -14,13 +9,6 @@ export type LabelElementProps = Omit<
   LabelHTMLAttributes<HTMLInputElement>,
   'color' | 'width' | 'height' | 'size'
 >;
-
-export type SpanElementProps = Omit<
-  HTMLAttributes<HTMLElement>,
-  'color' | 'translate'
-> &
-  RefAttributes<unknown>;
-
 export interface RadioButtonProps extends RadioButtonElementProps {
   label: string;
   name: string;
