349bd3392297123d1cf34ef4ef989eeb0837fac3
git-marcopitra <marcomorete73@hotmail.com>
2023-04-14 05:11:05 +0100

    :bookmark: faet: release new alpha version

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,7 @@
 export * from './button';
+export * from './list';
+export * from './list-item';
+export * from './progress-indicator';
 export * from './radio-button';
 export * from './switch';
 export * from './text-field';
