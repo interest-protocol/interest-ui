df03154c8f3f65ae01f9d12f0948caead357d0c1
Okusindja de Almeida <okusindja@gmail.com>
2023-04-24 17:08:31 +0100

    :bug: fix: slider ref

--- src/lib/elements/box/index.tsx
+++ src/lib/elements/box/index.tsx
@@ -1,10 +1,16 @@
 import stylin, { variant } from '@stylin.js/react';
-import React, { forwardRef, PropsWithChildren, RefAttributes } from 'react';
+import React, {
+  FC,
+  forwardRef,
+  PropsWithChildren,
+  PropsWithRef,
+  RefAttributes,
+} from 'react';
 
 import { BoxElementProps, BoxProps } from '../box/box.types';
 
-export const Box = forwardRef(
-  ({ as, ...props }: PropsWithChildren<BoxProps>, ref) => {
+export const Box: FC<PropsWithRef<PropsWithChildren<BoxProps>>> = forwardRef(
+  ({ as, ...props }, ref) => {
     const BoxElement = stylin<BoxElementProps & RefAttributes<unknown>>(
       as || 'div'
     )(
