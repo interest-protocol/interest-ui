a34ffa9d990f27c6aee344bfa731262a697bd755
git-marcopitra <marcomorete73@hotmail.com>
2023-06-04 21:21:18 +0100

    :sparkles: fix: on change slider and relative posittion

--- src/lib/components/slider/slider.types.ts
+++ src/lib/components/slider/slider.types.ts
@@ -4,11 +4,12 @@ export type InputElementProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
   'color' | 'translate' | 'height' | 'width'
 >;
+
 export interface SliderProps {
-  min: number;
   max: number;
-  value: number;
+  min?: number;
   step?: number;
   initial?: number;
   disabled?: boolean;
+  onChange: (value: number) => void;
 }
