ba7adc7d85f44779baaf848944a4928d3c3da2ec
git-marcopitra <marcomorete73@hotmail.com>
2023-06-14 17:29:51 +0100

    :recycle: refactor: improve story

--- src/lib/components/modal/stories/index.stories.tsx
+++ src/lib/components/modal/stories/index.stories.tsx
@@ -1,9 +1,36 @@
 import { Meta, StoryObj } from '@storybook/react';
-import React from 'react';
+import React, { FC, useState } from 'react';
 import { v4 } from 'uuid';
 
 import { Box } from '../../../elements';
-import { Modal } from '..';
+import { Modal as IUIModal } from '..';
+import { ModalProps } from '../modal.types';
+
+const Modal: FC<ModalProps> = (args) => {
+  const [isOpen, setIsOpen] = useState(args.isOpen);
+
+  return (
+    <IUIModal
+      {...args}
+      isOpen={isOpen}
+      onClose={() => {
+        setIsOpen(false);
+        args.onClose?.();
+      }}
+    >
+      {['Modal', 'Content'].map((text) => (
+        <Box
+          p="4xl"
+          borderTop="1px solid"
+          borderColor="outline.outline"
+          key={v4()}
+        >
+          {text}
+        </Box>
+      ))}
+    </IUIModal>
+  );
+};
 
 const meta: Meta<typeof Modal> = {
   title: 'Modal',
@@ -26,25 +53,12 @@ export default meta;
 
 type Story = StoryObj<typeof Modal>;
 
-const toggleModal = () => alert('close');
-
 export const Normal: Story = {
   args: {
-    isOpen: false,
+    isOpen: true,
     allowClose: true,
+    ariaHideApp: false,
     title: 'IPX Balance',
     hasCloseButton: true,
-    onClose: toggleModal,
-    ariaHideApp: false, // JUST FOR THE STORY - because we are not setting the app
-    children: ['Modal', 'Content'].map((text) => (
-      <Box
-        p="4xl"
-        borderTop="1px solid"
-        borderColor="outline.outline"
-        key={v4()}
-      >
-        {text}
-      </Box>
-    )),
   },
 };
