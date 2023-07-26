ba8d326e25aee5de52ce130127727927450b8ec5
Okusindja de Almeida <okusindja@gmail.com>
2023-07-27 00:02:57 +0100

    :sparkles: feat: add tooltip component

---
+++ src/lib/components/tooltip/index.tsx
@@ -0,0 +1,114 @@
+import { FC, PropsWithChildren, useState } from 'react';
+import React from 'react';
+
+import { Box, Motion, MotionProps } from '../../elements';
+import { TooltipProps } from './tooltip.types';
+
+export const TooltipWrapper: FC<
+  PropsWithChildren<TooltipProps & MotionProps>
+> = ({ children, tooltipContent, tooltipPosition, ...props }) => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Box
+      cursor="pointer"
+      position="relative"
+      onMouseEnter={() => {
+        setToggle(true);
+      }}
+      onMouseLeave={() => {
+        setToggle(false);
+      }}
+    >
+      {children}
+      {toggle && (
+        <Motion
+          layout
+          initial={{ opacity: 0.2 }}
+          animate={{
+            opacity: 1,
+          }}
+          position="absolute"
+          borderRadius=".25rem"
+          transform={`translate(${
+            tooltipPosition === 'top' || tooltipPosition === 'bottom'
+              ? '-50%'
+              : tooltipPosition === 'left'
+              ? '-115%'
+              : tooltipPosition === 'right'
+              ? '115%'
+              : '0'
+          }, ${
+            tooltipPosition === 'top'
+              ? '-115%'
+              : tooltipPosition === 'bottom'
+              ? '115%'
+              : tooltipPosition === 'left' || tooltipPosition === 'right'
+              ? '-50%'
+              : '0'
+          })`}
+          top={
+            tooltipPosition === 'top'
+              ? '0'
+              : tooltipPosition === 'left' || tooltipPosition === 'right'
+              ? '50%'
+              : 'unset'
+          }
+          left={
+            tooltipPosition === 'left'
+              ? '0'
+              : tooltipPosition === 'top' || tooltipPosition === 'bottom'
+              ? '50%'
+              : 'unset'
+          }
+          right={tooltipPosition === 'right' ? '0' : 'unset'}
+          bottom={tooltipPosition === 'bottom' ? '0' : 'unset'}
+          {...props}
+        >
+          <Box p=".5rem">{tooltipContent}</Box>
+          <Box
+            ml={
+              tooltipPosition === 'top' || tooltipPosition === 'bottom'
+                ? '50%'
+                : 'unset'
+            }
+            bg="inherit"
+            width=".375rem"
+            height=".375rem"
+            top={
+              tooltipPosition === 'bottom'
+                ? '-.1875rem'
+                : tooltipPosition === 'left' || tooltipPosition === 'right'
+                ? '50%'
+                : 'unset'
+            }
+            left={tooltipPosition === 'right' ? '-.1875rem' : 'unset'}
+            right={tooltipPosition === 'left' ? '-.1875rem' : 'unset'}
+            bottom={
+              tooltipPosition === 'top'
+                ? '-.1875rem'
+                : tooltipPosition === 'left' || tooltipPosition === 'right'
+                ? '0'
+                : 'unset'
+            }
+            position="absolute"
+            transform={`translate(${
+              tooltipPosition === 'top'
+                ? '-50%'
+                : tooltipPosition === 'left' || tooltipPosition === 'right'
+                ? '0'
+                : '-50%'
+            }, ${
+              tooltipPosition === 'top'
+                ? '0'
+                : tooltipPosition === 'left' || tooltipPosition === 'right'
+                ? '-50%'
+                : '0%'
+            }) rotate(45deg)`}
+          />
+        </Motion>
+      )}
+    </Box>
+  );
+};
+export type { TooltipProps };
---
+++ src/lib/components/tooltip/stories/index.stories.tsx
@@ -0,0 +1,76 @@
+import { Meta, StoryObj } from '@storybook/react';
+import React, { FC, PropsWithChildren } from 'react';
+
+import { Box, Typography } from '../../../elements';
+import { TooltipProps, TooltipWrapper } from '..';
+
+const Tooltip: FC<PropsWithChildren<TooltipProps>> = (args) => {
+  return (
+    <Box width="fit-content" p="6.25rem" bg="surface">
+      <TooltipWrapper bg="onSurface" color="surface" {...args}></TooltipWrapper>
+    </Box>
+  );
+};
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Tooltip',
+  component: Tooltip,
+  argTypes: {
+    tooltipPosition: {
+      options: ['top', 'left', 'right', 'bottom'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Top: Story = {
+  args: {
+    tooltipPosition: 'top',
+    tooltipContent: <Typography variant="medium">Tooltip</Typography>,
+    children: (
+      <Typography variant="medium" color="onSurface">
+        Hover Me
+      </Typography>
+    ),
+  },
+};
+
+export const Left: Story = {
+  args: {
+    tooltipPosition: 'left',
+    tooltipContent: <Typography variant="medium">Tooltip</Typography>,
+    children: (
+      <Typography variant="medium" color="onSurface">
+        Hover Me
+      </Typography>
+    ),
+  },
+};
+
+export const Right: Story = {
+  args: {
+    tooltipPosition: 'right',
+    tooltipContent: <Typography variant="medium">Tooltip</Typography>,
+    children: (
+      <Typography variant="medium" color="onSurface">
+        Hover Me
+      </Typography>
+    ),
+  },
+};
+
+export const Bottom: Story = {
+  args: {
+    tooltipPosition: 'bottom',
+    tooltipContent: <Typography variant="medium">Tooltip</Typography>,
+    children: (
+      <Typography variant="medium" color="onSurface">
+        Hover Me
+      </Typography>
+    ),
+  },
+};
