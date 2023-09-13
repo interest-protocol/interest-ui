78e8fbd35ece9884b3890be4b765d4fdc2ab4861
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2023-09-13 07:23:40 +0100

    :bookmark: v0.1.31-beta (#74)
    * 💄 add tooltip component (#67)
    
    * :sparkles: feat: add tooltip component
    
    * :sparkles: feat: add tooltip component
    
    * :bookmark: chore: new beta version
    
    * :sparkles: feat: adding top element in text-field (#69)
    
    * :bookmark: feat: new beta patch
    
    * :bug: radio button (#70)
    
    * :bug: fix: radio button
    
    * :bug: fix: radio button
    
    * :bookmark: chore: beta v0.1.29
    
    * :bug: fix: add value on the callback
    
    * :bookmark: chore: v0.1.30-beta
    
    * ✨ add checkbox component (#73)
    
    * ✨ feat: checkbox component
    
    * ✨ chore: change label color
    
    * :bookmark: chore: new beta version
    
    ---------
    
    Co-authored-by: Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
    Co-authored-by: Kipas Jr <34079870+KipandaJr@users.noreply.github.com>

---
+++ src/lib/components/checkbox/checkbox.types.ts
@@ -0,0 +1,17 @@
+import { InputHTMLAttributes, LabelHTMLAttributes } from 'react';
+
+export type CheckedButtonElementProps = Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'color' | 'defaultValue' | 'size'
+>;
+
+export type LabelElementProps = Omit<
+  LabelHTMLAttributes<HTMLInputElement>,
+  'color'
+>;
+export interface CheckboxProps extends CheckedButtonElementProps {
+  label: string;
+  name?: string;
+  onClick: () => void;
+  defaultValue: boolean;
+}
