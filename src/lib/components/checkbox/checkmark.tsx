9be441cf0d9b3465b9c2b9707373d3c7b6e088b8
Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
2023-09-13 07:14:16 +0100

    ✨ add checkbox component (#73)
    * ✨ feat: checkbox component
    
    * ✨ chore: change label color

---
+++ src/lib/components/checkbox/checkmark.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import React from 'react';
+
+import { SVGProps } from '../../icons/icons.types';
+import { Theme, useTheme } from '../../theme';
+
+export const Checkmark: FC<SVGProps> = ({ maxHeight, maxWidth }) => {
+  const { colors } = useTheme() as Theme;
+
+  return (
+    <svg width={maxWidth} height={maxHeight} viewBox="0 0 24 24">
+      <path
+        d="M10 16.4L6 12.4L7.4 11L10 13.6L16.6 7L18 8.4L10 16.4Z"
+        fill={colors['primary.onPrimary']}
+      />
+    </svg>
+  );
+};
+
+export default Checkmark;
