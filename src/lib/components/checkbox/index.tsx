c2e44f54076998290a383f2bbc36caaf080c2d03
KipandaJr <antoniokipandacardoso@gmail.com>
2023-11-18 23:39:01 +0100

    :bug: feat: solving a weird behavior

--- src/lib/components/checkbox/index.tsx
+++ src/lib/components/checkbox/index.tsx
@@ -67,69 +67,93 @@ export const Checkbox: FC<CheckboxProps> = ({
             : `${colors.primary}14`,
         }}
         borderRadius="full"
-      >
-        <Box position="relative" width="1.125rem" height="1.125rem">
-          <CheckedButtonElement
-            id={label}
-            name={name}
-            type="checkbox"
-            width="1.125rem"
-            height="1.125rem"
-            appearance="none"
-            onClick={handleClick}
-            borderRadius="0.25rem"
-            border={`${disabled ? 2 : 1}px solid`}
-            cursor={disabled ? 'not-allowed' : 'pointer'}
-            nHover={{
-              boxShadow: '0 0 .1875rem .3125rem' + colors['onPrimary'] + '1F',
-              transition: 'all 300ms ease-in-out',
-            }}
-            bg={
-              currentValue != 0
-                ? disabled
-                  ? '#0000003d'
-                  : colors.primary
-                : 'transparent'
-            }
-            borderColor={
-              currentValue != 0
-                ? disabled
-                  ? 'transparent'
-                  : colors.primary
-                : disabled
+      />
+      <Box position="relative" width="1.125rem" height="1.125rem">
+        <CheckedButtonElement
+          id={label}
+          name={name}
+          type="checkbox"
+          width="1.125rem"
+          height="1.125rem"
+          appearance="none"
+          onClick={handleClick}
+          borderRadius="0.25rem"
+          border="2px solid"
+          cursor={disabled ? 'not-allowed' : 'pointer'}
+          nHover={{
+            boxShadow: '0 0 .1875rem .3125rem' + colors['onPrimary'] + '1F',
+            transition: 'all 300ms ease-in-out',
+          }}
+          bg={
+            currentValue != 0
+              ? disabled
                 ? '#0000003d'
-                : 'onSurface'
-            }
-          />
-          <LabelElement
-            top="43%"
-            left="50%"
-            htmlFor={label}
-            position="absolute"
-            transform="translate(-50%, -43%)"
-            cursor={disabled ? 'not-allowed' : 'pointer'}
-            color={colors['onPrimary']}
+                : colors.primary
+              : 'transparent'
+          }
+          borderColor={
+            currentValue != 0
+              ? disabled
+                ? 'transparent'
+                : colors.primary
+              : disabled
+              ? '#0000003d'
+              : 'onSurface'
+          }
+        />
+        <LabelElement
+          top="43%"
+          left="50%"
+          htmlFor={label}
+          position="absolute"
+          transform="translate(-50%, -43%)"
+          cursor={disabled ? 'not-allowed' : 'pointer'}
+          color={colors['onPrimary']}
+        >
+          <Motion
+            initial="hidden"
+            variants={iconVariants}
+            animate={currentValue != 0 ? 'visible' : 'hidden'}
+            height="1rem"
           >
-            <Motion
-              initial="hidden"
-              variants={iconVariants}
-              animate={currentValue != 0 ? 'visible' : 'hidden'}
-              height="1rem"
-            >
-              <Checkmark
-                width="100%"
-                maxWidth="1rem"
-                maxHeight="1rem"
-                isIndeterminated={allowIndetermatedValue && currentValue == -1}
-              />
-            </Motion>
-          </LabelElement>
-        </Box>
+            <Checkmark
+              width="100%"
+              maxWidth="1rem"
+              maxHeight="1rem"
+              isIndeterminated={allowIndetermatedValue && currentValue == -1}
+            />
+          </Motion>
+        </LabelElement>
+        <Box
+          bg="transparent"
+          nHover={{
+            bg: disabled
+              ? 'unset'
+              : currentValue == 0
+              ? '#00000014'
+              : `${colors.primary}14`,
+          }}
+          nFocus={{
+            bg: disabled
+              ? 'unset'
+              : currentValue == 0
+              ? '#00000029'
+              : `${colors.primary}29`,
+          }}
+          top="-0.45rem"
+          left="-0.45rem"
+          width="2rem"
+          height="2rem"
+          position="absolute"
+          borderRadius="full"
+          onClick={handleClick}
+          transition="all 300ms ease-in-out"
+        />
       </Box>
       <LabelElement
         htmlFor={label}
         color="onSurface"
-        cursor={disabled ? 'not-allowed' : 'default'}
+        cursor={disabled ? 'not-allowed' : 'pointer'}
       >
         <Box>
           <Typography variant="body" size="large">
