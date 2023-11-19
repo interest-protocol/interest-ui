c5ee1654b05a7bb36536d1ac53f0125cc5ac0023
KipandaJr <antoniokipandacardoso@gmail.com>
2023-11-19 01:56:55 +0100

    :lipstick: fix: refactor effect on buttons

--- src/lib/components/button/index.tsx
+++ src/lib/components/button/index.tsx
@@ -1,8 +1,9 @@
 import stylin, { variant } from '@stylin.js/react';
-import { CustomDomComponent, easeInOut, motion } from 'framer-motion';
-import React, { FC, PropsWithChildren } from 'react';
+import { CustomDomComponent, motion } from 'framer-motion';
+import React, { FC, PropsWithChildren, useState } from 'react';
 
 import { Box } from '../../elements';
+import { Theme, useTheme } from '../../theme';
 import { ButtonElementProps, ButtonProps, NoIconButton } from './button.types';
 import { isIconButton } from './button.utils';
 
@@ -19,30 +20,43 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = ({
   children,
   ...props
 }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const { colors } = useTheme() as Theme;
+
   if (isIconButton(props))
     return (
       <MotionButton
         p="xs"
-        whileTap={{
-          scale: props.disabled ? 1 : 0.97,
-          transition: { duration: 0.005, ease: easeInOut },
-        }}
-        whileHover={{
-          scale: props.disabled ? 1 : 1.05,
-          transition: { duration: 0.005, ease: easeInOut },
-        }}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         {...props}
+        width="1.5rem"
+        height="1.5rem"
+        alignItems="center"
+        display="inline-flex"
+        justifyContent="center"
+        position="relative"
       >
+        {children}
         <Box
-          as="span"
-          width="1.5rem"
-          height="1.5rem"
-          alignItems="center"
-          display="inline-flex"
-          justifyContent="center"
-        >
-          {children}
-        </Box>
+          p="s"
+          nActive={
+            props.disabled
+              ? {}
+              : {
+                  border: '0.25rem solid',
+                  borderColor: `${colors.primary}29`,
+                }
+          }
+          position="absolute"
+          borderRadius="full"
+          transition="all 300ms ease-in-out"
+          border={isFocused ? '0.25rem solid' : 'unset'}
+          left={props.variant == 'outline' ? '-11%' : '-10%'}
+          width={props.variant == 'outline' ? '122%' : '120%'}
+          height={props.variant == 'outline' ? '128%' : '120%'}
+          borderColor={isFocused ? `${colors.primary}29` : 'transparent'}
+        />
       </MotionButton>
     );
 
@@ -53,21 +67,33 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = ({
       py="xs"
       pr={SuffixIcon ? 'm' : 'xl'}
       pl={PrefixIcon ? 'm' : 'xl'}
-      whileTap={{
-        scale: props.disabled ? 1 : 0.97,
-        transition: { duration: 0.005, ease: easeInOut },
-      }}
-      whileHover={{
-        scale: props.disabled ? 1 : 1.05,
-        transition: { duration: 0.005, ease: easeInOut },
-      }}
+      position="relative"
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
       {...props}
     >
-      <>
-        {PrefixIcon}
-        {children}
-        {SuffixIcon}
-      </>
+      {PrefixIcon}
+      {children}
+      {SuffixIcon}
+      <Box
+        p="s"
+        nActive={
+          props.disabled
+            ? {}
+            : {
+                border: '0.25rem solid',
+                borderColor: `${colors.primary}29`,
+              }
+        }
+        position="absolute"
+        borderRadius="full"
+        transition="all 300ms ease-in-out"
+        border={isFocused ? '0.25rem solid' : 'unset'}
+        left={props.variant == 'outline' ? '-4%' : '-3%'}
+        width={props.variant == 'outline' ? '107.7%' : '106%'}
+        height={props.variant == 'outline' ? '128%' : '120%'}
+        borderColor={isFocused ? `${colors.primary}29` : 'transparent'}
+      />
     </MotionButton>
   );
 };
