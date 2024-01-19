52f7ac0abaf7c5d535a16a583efcb1b0b7bf556a
MarioBatalha <mariobatalha9@gmail.com>
2024-01-19 13:46:37 +0200

    :construction: fix: change slider lib

--- src/lib/components/slider/index.tsx
+++ src/lib/components/slider/index.tsx
@@ -1,23 +1,16 @@
 import { useTheme } from '@emotion/react';
 import React, { FC, useState } from 'react';
-import { getTrackBackground, Range } from 'react-range';
 
 import { Box } from '../../elements';
 import { Theme } from '../../theme';
-import { RangeType } from './range-type';
 import { SliderProps } from './slider.types';
-import { ThumbWrapper } from './thumb-wrapper';
 
 const SliderElement: FC<SliderProps> = ({
   min = 0,
-  step = 1,
   onChange,
   max = 100,
   initial = 0,
-  withTooltip,
   disabled = false,
-  bottomValue = false,
-  showZeroValue = false,
 }) => {
   const { colors } = useTheme() as Theme;
   const [values, setValues] = useState(
@@ -30,60 +23,13 @@ const SliderElement: FC<SliderProps> = ({
   };
 
   return (
-    <Range
-      max={max}
-      min={min}
-      step={step}
+    <RangeSlider
+      className="single-thumb"
+      defaultValue={[min, max]}
+      thumbsDisabled={[true, false]}
+      rangeSlideDisabled={disabled}
       values={values}
-      disabled={disabled}
-      allowOverlap
       onChange={handleChange}
-      renderTrack={({ props, children }) => (
-        <Box
-          width="100%"
-          display="flex"
-          height="2.25rem"
-          marginTop="1.5rem"
-          alignItems="center"
-          onMouseDown={props.onMouseDown}
-          onTouchStart={props.onTouchStart}
-        >
-          <Box
-            width="100%"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            ref={props.ref}
-            height=".25rem"
-            alignSelf="center"
-            background={getTrackBackground({
-              max: max,
-              min: min || 0,
-              values: values,
-              colors:
-                values.length == 1
-                  ? [`${colors.primary}`, 'rgba(0, 0, 0, 0.08)']
-                  : [
-                      'rgba(0, 0, 0, 0.08)',
-                      `${colors.primary}`,
-                      'rgba(0, 0, 0, 0.08)',
-                    ],
-            })}
-          >
-            {children}
-          </Box>
-        </Box>
-      )}
-      renderThumb={({ props }) => (
-        <ThumbWrapper disabled={disabled} {...props}>
-          <RangeType
-            disabled={disabled}
-            value={values[props.key]}
-            withTooltip={withTooltip}
-            bottomValue={bottomValue}
-            showZeroValue={showZeroValue}
-          />
-        </ThumbWrapper>
-      )}
     />
   );
 };
