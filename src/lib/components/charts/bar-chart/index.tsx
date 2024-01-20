78cccf2b2a024bed3a1ce641a8c8c4d833d0980b
Atila <atilaesousa@gmail.com>
2024-01-21 00:15:08 +0100

    :bug: fix: unnecessary return

--- src/lib/components/charts/bar-chart/index.tsx
+++ src/lib/components/charts/bar-chart/index.tsx
@@ -14,40 +14,34 @@ import CustomTooltip from '../tooltip';
 import CustomCursor from './custom-cursor';
 import CustomXAxisTick from './x-axis-tick';
 
-const BarChartComponent: FC<BaseChartProps> = ({ data, height, width }) => {
-  return (
-    <ResponsiveContainer width={width || '100%'} height={height || 200}>
-      <BarChart
-        data={data}
-        barCategoryGap="1%"
-        margin={{
-          top: 5,
-          left: 20,
-          right: 30,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid
-          opacity={0.25}
-          vertical={false}
-          stroke="outlineVariant"
-        />
-        <XAxis
-          type="category"
-          dataKey="date"
-          tickLine={false}
-          tick={<CustomXAxisTick />}
-          interval="preserveStartEnd"
-        />
-        <Tooltip
-          cursor={<CustomCursor />}
-          animationEasing="ease-in-out"
-          content={<CustomTooltip />}
-        />
-        <Bar dataKey="amount" fill={colors['primary']} />
-      </BarChart>
-    </ResponsiveContainer>
-  );
-};
+const BarChartComponent: FC<BaseChartProps> = ({ data, height, width }) => (
+  <ResponsiveContainer width={width || '100%'} height={height || 200}>
+    <BarChart
+      data={data}
+      barCategoryGap="1%"
+      margin={{
+        top: 5,
+        left: 20,
+        right: 30,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid opacity={0.25} vertical={false} stroke="outlineVariant" />
+      <XAxis
+        type="category"
+        dataKey="date"
+        tickLine={false}
+        tick={<CustomXAxisTick />}
+        interval="preserveStartEnd"
+      />
+      <Tooltip
+        cursor={<CustomCursor />}
+        animationEasing="ease-in-out"
+        content={<CustomTooltip />}
+      />
+      <Bar dataKey="amount" fill={colors['primary']} />
+    </BarChart>
+  </ResponsiveContainer>
+);
 
 export default BarChartComponent;
