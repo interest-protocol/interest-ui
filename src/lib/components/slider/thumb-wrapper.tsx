2b7af433b6a2dc51753902c1cd6535c47cda0df4
KipandaJr <antoniokipandacardoso@gmail.com>
2024-02-06 02:47:31 +0100

    :bug: fix: solving getBoundingClientRect bug

--- src/lib/components/slider/thumb-wrapper.tsx
+++ src/lib/components/slider/thumb-wrapper.tsx
@@ -3,24 +3,26 @@ import React, { FC, PropsWithChildren } from 'react';
 import { Box } from '../../elements';
 import { ThumbWrapperProps } from './slider.types';
 
-export const ThumbWrapper: FC<PropsWithChildren<ThumbWrapperProps>> = (
-  props
-) => (
+export const ThumbWrapper: FC<PropsWithChildren<ThumbWrapperProps>> = ({
+  children,
+  disabled,
+  thumbDetails,
+}) => (
   <Box
-    {...props}
+    {...thumbDetails}
     display="flex"
     width="1.25rem"
     height="1.25rem"
     borderRadius="50%"
     border="1px solid"
     alignItems="center"
-    borderColor={!props.disabled ? 'primary' : `#C8C8C9`}
     justifyContent="center"
-    cursor={props.disabled ? 'not-allowed !important' : 'pointer'}
-    backgroundColor={!props.disabled ? 'lowestContainer' : `#C8C8C9`}
+    borderColor={!disabled ? 'primary' : `#C8C8C9`}
+    cursor={disabled ? 'not-allowed !important' : 'pointer'}
+    backgroundColor={!disabled ? 'lowestContainer' : `#C8C8C9`}
     boxShadow="0px 2px 4px -2px rgba(13, 16, 23, 0.04), 0px 4px 8px -2px rgba(13, 16, 23, 0.12)"
     nHover={
-      !props.disabled && {
+      !disabled && {
         border: '1.33px solid',
         bg: 'primaryContainer',
         borderColor: 'primary',
@@ -28,7 +30,7 @@ export const ThumbWrapper: FC<PropsWithChildren<ThumbWrapperProps>> = (
       }
     }
     nFocus={
-      !props.disabled && {
+      !disabled && {
         border: '1px solid',
         bg: 'primaryContainer',
         borderColor: 'primary',
@@ -37,7 +39,7 @@ export const ThumbWrapper: FC<PropsWithChildren<ThumbWrapperProps>> = (
     }
     position="relative"
   >
-    {props.children}
+    {children}
   </Box>
 );
 
