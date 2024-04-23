dc7a1852906604b7bae384b13d7aba6a3fb355ac
Mario Batalha <mariobatalha9@gmail.com>
2024-04-23 18:14:15 +0100

    :test_tube: test: modal interactions test

--- src/lib/components/modal/index.tsx
+++ src/lib/components/modal/index.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import { FC } from 'react';
 import ReactModal from 'react-modal';
 
+import { Box } from '../../elements';
 import { ModalProps } from './modal.types';
 import ModalContent from './modal-content';
 
@@ -17,33 +18,35 @@ export const Modal: FC<PropsWithChildren<ModalProps>> = ({
   const background = `rgba(0,0,0,${opaque ? 1 : 0.5})`;
 
   return (
-    <ReactModal
-      {...props}
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      shouldCloseOnEsc={allowClose}
-      shouldCloseOnOverlayClick={allowClose}
-      style={{
-        overlay: {
-          background,
-          zIndex: 99999,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        content: {
-          padding: 0,
-          border: 'none',
-          maxWidth: '95vw',
-          maxHeight: '95vh',
-          borderRadius: '0',
-          position: 'static',
-          overflow: 'visible',
-          background: 'transparent',
-        },
-      }}
-    >
-      <ModalContent isOpen={isOpen} onClose={onClose} {...props} />
-    </ReactModal>
+    <Box data-testid="modal">
+      <ReactModal
+        {...props}
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        shouldCloseOnEsc={allowClose}
+        shouldCloseOnOverlayClick={allowClose}
+        style={{
+          overlay: {
+            background,
+            zIndex: 99999,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+          content: {
+            padding: 0,
+            border: 'none',
+            maxWidth: '95vw',
+            maxHeight: '95vh',
+            borderRadius: '0',
+            position: 'static',
+            overflow: 'visible',
+            background: 'transparent',
+          },
+        }}
+      >
+        <ModalContent isOpen={isOpen} onClose={onClose} {...props} />
+      </ReactModal>
+    </Box>
   );
 };
