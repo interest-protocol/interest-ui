8fc29ece7762b5d94af3a6ed7b45dc574478f0f7
Mario Batalha <mariobatalha9@gmail.com>
2024-06-28 14:04:23 +0100

    :test_tube: test: add new checkbox test

--- src/lib/components/checkbox/stories/index.stories.tsx
+++ src/lib/components/checkbox/stories/index.stories.tsx
@@ -21,24 +21,33 @@ export const Normal: Story = {
   },
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
-
     const checkbox = canvas.getByRole('checkbox');
     const computedStyle = getComputedStyle(checkbox);
-    const border = computedStyle.getPropertyValue('border');
     const color = computedStyle.getPropertyValue('color');
+    const border = computedStyle.getPropertyValue('border');
     const background = computedStyle.getPropertyValue('background');
+    const transition = computedStyle.getPropertyValue('transition');
+    const borderRadius = computedStyle.getPropertyValue('border-radius');
 
-    await step('Text field onClick', async () => {
-      await userEvent.click(canvas.getByRole('checkbox'));
-    });
-
-    await step('Check property value and args', () => {
-      expect(args.disabled).toBeFalsy();
-      expect(args.defaultValue).toBeFalsy();
+    await step('Checkbox style test', async () => {
+      expect(borderRadius).toBe('4px');
       expect(color.trim()).toBe('rgb(0, 0, 0)');
-      expect(args.label).toBe('Checkbox Label');
+      expect(transition).toBe('all 0s ease 0s');
       expect(border.trim()).toBe('2px solid rgb(27, 27, 31)');
       expect(background.substring(0, 16)).toBe('rgba(0, 0, 0, 0)');
+    });
+
+    await step('Checkbox useEvent test', async () => {
+      await userEvent.click(canvas.getByRole('checkbox'));
+      await userEvent.hover(canvas.getByRole('checkbox'));
+      await userEvent.unhover(canvas.getByRole('checkbox'));
+      await userEvent.dblClick(canvas.getByRole('checkbox'));
+    });
+
+    await step('Checkbox args test', async () => {
+      expect(args.disabled).toBeFalsy();
+      expect(args.defaultValue).toBeFalsy();
+      expect(args.label).toBe('Checkbox Label');
     });
   },
 };
@@ -49,23 +58,32 @@ export const NormalWithoutLabel: Story = {
   },
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
-
     const checkbox = canvas.getByRole('checkbox');
     const computedStyle = getComputedStyle(checkbox);
-    const border = computedStyle.getPropertyValue('border');
     const color = computedStyle.getPropertyValue('color');
+    const border = computedStyle.getPropertyValue('border');
     const background = computedStyle.getPropertyValue('background');
+    const transition = computedStyle.getPropertyValue('transition');
+    const borderRadius = computedStyle.getPropertyValue('border-radius');
 
-    await step('Text field onClick', async () => {
-      await userEvent.click(canvas.getByRole('checkbox'));
-    });
-
-    await step('Check property value and args', () => {
-      expect(args.disabled).toBeFalsy();
-      expect(args.defaultValue).toBeTruthy();
+    await step('Checkbox style test', async () => {
+      expect(borderRadius).toBe('4px');
       expect(color.trim()).toBe('rgb(0, 0, 0)');
+      expect(transition).toBe('all 0s ease 0s');
       expect(border.trim()).toBe('2px solid rgb(0, 83, 219)');
       expect(background.substring(0, 16)).toBe('rgb(0, 83, 219) ');
+    });
+
+    await step('Checkbox useEvent test', async () => {
+      await userEvent.click(canvas.getByRole('checkbox'));
+      await userEvent.hover(canvas.getByRole('checkbox'));
+      await userEvent.unhover(canvas.getByRole('checkbox'));
+      await userEvent.dblClick(canvas.getByRole('checkbox'));
+    });
+
+    await step('Checkbox args test', async () => {
+      expect(args.disabled).toBeFalsy();
+      expect(args.defaultValue).toBeTruthy();
     });
   },
 };
@@ -79,24 +97,33 @@ export const NormalWithIndeterminate: Story = {
   },
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
-
     const checkbox = canvas.getByRole('checkbox');
     const computedStyle = getComputedStyle(checkbox);
-    const border = computedStyle.getPropertyValue('border');
     const color = computedStyle.getPropertyValue('color');
+    const border = computedStyle.getPropertyValue('border');
     const background = computedStyle.getPropertyValue('background');
+    const transition = computedStyle.getPropertyValue('transition');
+    const borderRadius = computedStyle.getPropertyValue('border-radius');
 
-    await step('Checkbox onClick', async () => {
-      await userEvent.click(canvas.getByRole('checkbox'));
-    });
-
-    await step('Check property value and args', () => {
-      expect(args.disabled).toBeFalsy();
-      expect(args.defaultValue).toBeFalsy();
+    await step('Checkbox style test', async () => {
+      expect(borderRadius).toBe('4px');
       expect(color.trim()).toBe('rgb(0, 0, 0)');
-      expect(args.label).toBe('Checkbox Label');
+      expect(transition).toBe('all 0s ease 0s');
       expect(border.trim()).toBe('2px solid rgb(27, 27, 31)');
       expect(background.substring(0, 16)).toBe('rgba(0, 0, 0, 0)');
+    });
+
+    await step('Checkbox useEvent test', async () => {
+      await userEvent.click(canvas.getByRole('checkbox'));
+      await userEvent.hover(canvas.getByRole('checkbox'));
+      await userEvent.unhover(canvas.getByRole('checkbox'));
+      await userEvent.dblClick(canvas.getByRole('checkbox'));
+    });
+
+    await step('Checkbox args test', async () => {
+      expect(args.disabled).toBeFalsy();
+      expect(args.defaultValue).toBeFalsy();
+      expect(args.label).toBe('Checkbox Label');
       expect(args.allowIndeterminateValue).toBeTruthy();
     });
   },
@@ -112,26 +139,35 @@ export const NormalWithIndeterminateAndSupportText: Story = {
   },
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
-
     const checkbox = canvas.getByRole('checkbox');
     const computedStyle = getComputedStyle(checkbox);
     const color = computedStyle.getPropertyValue('color');
     const border = computedStyle.getPropertyValue('border');
     const background = computedStyle.getPropertyValue('background');
+    const transition = computedStyle.getPropertyValue('transition');
+    const borderRadius = computedStyle.getPropertyValue('border-radius');
 
-    await step('Checkbox onClick', async () => {
-      await userEvent.click(canvas.getByRole('checkbox'));
+    await step('Checkbox style test', async () => {
+      expect(borderRadius).toBe('4px');
+      expect(transition).toBe('all 0s ease 0s');
+      expect(color.trim()).toBe('rgb(0, 0, 0)');
+      expect(border.trim()).toBe('2px solid rgb(27, 27, 31)');
+      expect(background.substring(0, 16)).toBe('rgba(0, 0, 0, 0)');
     });
 
-    await step('Check property value and args', () => {
+    await step('Checkbox useEvent test', async () => {
+      await userEvent.click(canvas.getByRole('checkbox'));
+      await userEvent.hover(canvas.getByRole('checkbox'));
+      await userEvent.unhover(canvas.getByRole('checkbox'));
+      await userEvent.dblClick(canvas.getByRole('checkbox'));
+    });
+
+    await step('Checkbox args test', async () => {
       expect(args.disabled).toBeFalsy();
       expect(args.defaultValue).toBeFalsy();
-      expect(color.trim()).toBe('rgb(0, 0, 0)');
       expect(args.label).toBe('Checkbox Label');
       expect(args.allowIndeterminateValue).toBeFalsy();
       expect(args.supportingText).toBe('Supporting Text');
-      expect(border.trim()).toBe('2px solid rgb(27, 27, 31)');
-      expect(background.substring(0, 16)).toBe('rgba(0, 0, 0, 0)');
     });
   },
 };
@@ -144,24 +180,33 @@ export const NormalDisabled: Story = {
   },
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
-
     const checkbox = canvas.getByRole('checkbox');
     const computedStyle = getComputedStyle(checkbox);
     const color = computedStyle.getPropertyValue('color');
     const border = computedStyle.getPropertyValue('border');
     const background = computedStyle.getPropertyValue('background');
+    const transition = computedStyle.getPropertyValue('transition');
+    const borderRadius = computedStyle.getPropertyValue('border-radius');
 
-    await step('Checkbox onClick', async () => {
-      await userEvent.click(canvas.getByRole('checkbox'));
-    });
-
-    await step('Check property value and args', () => {
-      expect(args.disabled).toBeTruthy();
-      expect(args.defaultValue).toBeTruthy();
+    await step('Checkbox style test', async () => {
+      expect(borderRadius).toBe('4px');
       expect(color.trim()).toBe('rgb(0, 0, 0)');
-      expect(args.label).toBe('Checkbox Label');
+      expect(transition).toBe('all 0s ease 0s');
       expect(border.trim()).toBe('2px solid rgba(0, 0, 0, 0)');
       expect(background.substring(0, 19)).toBe('rgba(0, 0, 0, 0.24)');
+    });
+
+    await step('Checkbox useEvent test', async () => {
+      await userEvent.click(canvas.getByRole('checkbox'));
+      await userEvent.hover(canvas.getByRole('checkbox'));
+      await userEvent.unhover(canvas.getByRole('checkbox'));
+      await userEvent.dblClick(canvas.getByRole('checkbox'));
+    });
+
+    await step('Checkbox args test', async () => {
+      expect(args.disabled).toBeTruthy();
+      expect(args.defaultValue).toBeTruthy();
+      expect(args.label).toBe('Checkbox Label');
     });
   },
 };
