e3f59d4d46d89589b36dc436ab7175238b1545b5
Atila <atilaesousa@gmail.com>
2025-02-05 22:59:03 +0100

    :recycle: feat: refactor tooltip

--- src/lib/components/tooltip/tooltip.types.ts
+++ src/lib/components/tooltip/tooltip.types.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import { MotionProps } from '../../elements';
 
 export interface TooltipProps extends MotionProps {
+  tooltipContentMaxWidth?: string;
   tooltipContent: string | ReactNode;
   tooltipPosition: 'top' | 'bottom' | 'left' | 'right';
 }
